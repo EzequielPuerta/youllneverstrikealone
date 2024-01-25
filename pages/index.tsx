a647350346b695c334b6b76f3f47513ee385a580
Ezequiel Puerta <eze19.2009@gmail.com>
2024-01-25 11:00:56 -0300

    :wrench: vercel analitics

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import App from "./App";
-
+import { Analytics } from '@vercel/analytics/react';
 
 export default function Home() {
   return (
@@ -12,6 +12,7 @@ export default function Home() {
 
       <main>
         <App />
+        <Analytics />
       </main>
 
       <footer>
